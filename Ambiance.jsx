5d55ba08bbdc23bc88a3f89f0c918d5a7d5da96d
Pao-La <73079863+Pao-La-CCC@users.noreply.github.com>
2022-07-04 16:36:02 +0200

    Add files via upload

---
+++ Ambiance.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+
+
+export default function AmbianceIcone(props) {
+
+
+    return(
+        <>
+         <svg width={props.width} height={props.height}  viewBox="0 0 505.5 215" fill="none"  id="Calque_2" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" ><defs>
+         </defs><rect class="cls-1" x="405" y="37" width="100" height="100"/><image width="27" height="27" transform="translate(38.5 21.5)" xlink:href="22ambiance.png"/></svg>
+        </>
+    )
+    
+}
+
+
+
+
+
+
+
+
+
+
